651411035e3201d5ddcfe19fb0bf672a11db9376
Kevin Kwa <kevincodes0430@gmail.com>
2026-05-11 07:50:37 +0800

    Fix register null loyalty point

--- booking/src/main/java/com/hotel/booking/model/User.java
+++ booking/src/main/java/com/hotel/booking/model/User.java
@@ -33,8 +33,8 @@ public class User {
     @CreationTimestamp
     private LocalDateTime createdAt = LocalDateTime.now();
 
-    @Column(nullable = false, columnDefinition = "int8 default 50")
-    private Long loyaltyPoint;
+    @Column(nullable = false)
+    private Long loyaltyPoint = 50L;
 
     public enum Role { 
         CUSTOMER, 
